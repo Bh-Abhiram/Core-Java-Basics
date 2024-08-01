a5d3948d4692a5d7d9485af5df42d93646252819
AbhiramBhimaraju@178 <131979662+Bh-Abhiram@users.noreply.github.com>
2024-08-01 22:22:01 +0530

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+class HelloJava
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Java!!");
+		System.out.println("Java is easy to learn!!");
+		System.out.println("Lets Explore java programming with it's core and advanced concepts");
+	}
+}
